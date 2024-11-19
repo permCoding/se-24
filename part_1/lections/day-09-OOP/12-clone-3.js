c33a99e20ff0fe7d64dca17bf12eca743e76e9af
Андрей Беляков <ttxiom@gmail.com>
2024-11-19 18:58:02 +0500

    up

--- part_1/lections/day-09-OOP/12-clone-3.js
+++ part_1/lections/day-09-OOP/12-clone-3.js
@@ -1,7 +1,7 @@
 // npm install lodash
 // глубокое клонирование 
 
-const _ = require('lodash');
+// const _ = require('lodash');
 
 let source = {
     id: 102,
@@ -12,7 +12,10 @@ let source = {
     }
 };
 
-let target = _.cloneDeep(source); // клонируем объект
+
+// let target = _.cloneDeep(source)
+// let target = structuredClone(source)
+let target = JSON.parse(JSON.stringify(source))
 
 source.id += 1; // все поля независимы
 source.private.age = 50; // все поля независимы
@@ -20,3 +23,20 @@ source.private.gender = false; // все поля независимы
 
 console.log('source =', source);
 console.log('target =', target);
+
+/*
+У метода JSON.parse(JSON.stringify(source)) есть ограничение:
+  — копируемые данные должны быть сериализуемы.
+
+Вот примеры несериализуемых данных: 
+  - примитив undefined, функция, symbol 
+  - при вызове JSON.stringify получаем undefined
+
+Массивы и объекты - сериализуемы. 
+Что будет если у них в качестве ключа или значения будут несериализуемые данные?
+
+- для массивов: такие значения будут превращены в null;
+- для объектов: такие значения будут опущены, 
+  а если symbol является ключом объекта, 
+  то он будет проигнорирован, даже при использовании функции replacer.
+*/
