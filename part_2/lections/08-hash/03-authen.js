21fa5b187b8a8adc6cab0c523397709c1670b776
Андрей Беляков <ttxiom@gmail.com>
2025-04-27 15:23:50 +0500

    up

--- part_2/lections/08-hash/03-authen.js
+++ part_2/lections/08-hash/03-authen.js
@@ -2,7 +2,9 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
 
-// из этого сделать ejs
+// добавить файл style.css
+
+// из этого сделать login.ejs
 const htmlLogin = `
     <form method="POST" action="/login">
         <input type="text" name="username" placeholder="Логин">
@@ -10,9 +12,14 @@ const htmlLogin = `
         <button type="submit">Войти</button>
     </form>`;
 
+// из этого сделать dashboard.ejs
 const personalAccount = `
     <h1>Личный кабинет</h1>
-    <a href="/logout">Выйти</a>`;
+    <h3>меню 1</h3>
+    <h3>меню 2</h3>
+    <h3>меню 3</h3>
+    <br>
+    <h4><a href="/logout">Выйти</a></h4>`;
 
 // = = = = = = = = = 
 
@@ -20,7 +27,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 const checkAuth = (req, res, next) => {
-    if (!req.cookies.accessToken) { // Middleware проверки авторизации
+    if (!req.cookies.accessToken) { // Middleware для аутентификации
         return res.redirect('/login');
     }
     next();
@@ -37,7 +44,7 @@ app.post('/login', (req, res) => {
     if (username === 'admin' && password === 'secret') {
         res.cookie('accessToken', 'secure_token_admin', {
             httpOnly: true,
-            maxAge: 1 * 60 * 60 * 1000 // на один час
+            maxAge: 2 * 60 * 60 * 1000 // на один час
         });
         return res.redirect('/dashboard');
     }
@@ -45,13 +52,33 @@ app.post('/login', (req, res) => {
     res.redirect('/login?error=1');
 });
 
+// из этого сделать роутер для пути /dashboard
 app.get('/dashboard', checkAuth, (req, res) => {
     res.send(personalAccount); // Защищенная страница
 });
 
+// из этого сделать роутер для выхода из личного кабинета
 app.get('/logout', (req, res) => { // Выход
     res.clearCookie('accessToken');
     res.redirect('/login');
 });
 
 app.listen(3000, () => console.log(`http://localhost:3000/`));
+
+/**
+ * проект 1 - Личный кабинет - это задание на лабораторку
+ * 
+ * выполнить проект с аутентификацией пользователя через куки браузера
+ * данные пусть хранятся в базе данных SQLite, БД создайте заранее
+ * и заранее впишите туда userLogin и userPassword
+ * только пароль пусть хранится в виде хэша
+ * то есть в этом проекте не будет возможности у пользователя пройти регистрацию
+ * зарегистрировать пользователя может только админ, сам вписав его логин и пароль
+ * путь в БД хранится не только логин и пароль пользователя, но и его Фамилия
+ * и после аутентификации пусть в личном кабинете отображается его Фамилия
+ * создайте необходимые модули с Роутерами, Представлениями и файл с таблицей стилей css
+ *  
+ * для реализации можно использовать:
+ * - файлы из текущей Лекции (для хэширования и сравнения паролей, для аутентификации)
+ * - файлы из предыдущих Лекций (где была реализация паттерна MVC)
+ */
